b3f184f88d3811ab0e3fba8952400b7298bbf054
Krystal Nguyen <krystal.tt.nguyen@outlook.com>
2025-02-17 12:34:20 +0100

    Add MapGet och MapPost

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
     config.DocumentName = "Minimal API";
-    config.Title = "MinimalAPI v1";
+    config.Title = "Minimal API v1";
     config.Version = "v1";
 });
 
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
     app.UseOpenApi();
     app.UseSwaggerUi(config =>
         {
-        config.DocumentTitle = "MinimalAPI";
+        config.DocumentTitle = "Minimal API";
         config.Path = "/swagger";
         config.DocumentPath = "/swagger/{documentName}/swagger.json";
         config.DocExpansion = "list";
@@ -24,4 +24,36 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Hello World!");
 
+var users = new List<User>() {
+   new User {Id = 1, Name = "Anna"},
+   new User {Id = 2, Name = "Bob"},
+   new User {Id = 3, Name = "Charlie"}
+};
+
+app.MapGet("/users", () => users);
+app.MapGet("/users/{id}", (int id) => {
+    var user = users.FirstOrDefault(u => u.Id == id);
+    return user is not null ? Results.Ok(user.Name) : Results.NotFound($"Unable to find user with ID: {id}.");
+});
+
+app.MapPost("/users", (User user) => {
+    if (users.Any(u => u.Id == user.Id))
+        return Results.BadRequest($"UserId: {user.Id} is occupied, use another Id.");
+    
+    users.Add(user);
+    return Results.Created($"/users/{user.Id}", user);
+});
+
+
+// app.MapGet("/product", () => product);
+// app.MapGet("/order", () => order);
+// app.MapGet("/category", () => category);
+
 app.Run();
+
+
+
+public class User {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
